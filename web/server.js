e0d20dc09a48591858e82190963ec08c2789a2df
stephanLeece <stephan.leece@outlook.com>
2018-04-21 16:16:12 +0200

    area select map

--- web/server.js
+++ web/server.js
@@ -6,7 +6,7 @@ server.use(express.static(__dirname + `/public`));
 
 server.get('/', function(req, res) {
   console.log(__dirname);
-    res.sendFile(__dirname + '/public/list.html');
+    res.sendFile(__dirname + '/public/selectArea.html');
 });
 
 
