c3a3bbebf63751e838c4e2ef32c9cef90558d18d
stephanLeece <stephan.leece@outlook.com>
2018-04-21 17:43:25 +0200

    map getting users location

--- web/public/hereMap.js
+++ web/public/hereMap.js
@@ -1,18 +1,50 @@
-var platform = new H.service.Platform({
-  'app_id': 'TkZ5D3cR7A0TNOTm2YI6',
-  'app_code': 'KUbzQcWdvzlcVJrFSQabLQ'
-});
+var latitude;
+var longitude
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      var platform = new H.service.Platform({
+        'app_id': 'TkZ5D3cR7A0TNOTm2YI6',
+        'app_code': 'KUbzQcWdvzlcVJrFSQabLQ'
+      });
 
-var defaultLayers = platform.createDefaultLayers();
+      var defaultLayers = platform.createDefaultLayers();
 
-// Instantiate (and display) a map object:
-var map = new H.Map(
-  document.getElementById('hereMap'),
-  defaultLayers.normal.map,
-  {
-    zoom: 11,
-    center: { lat: 31.432793, lng: 34.387282 }
-  });
+      // Instantiate (and display) a map object:
+      var map = new H.Map(
+        document.getElementById('hereMap'),
+        defaultLayers.normal.map,
+        {
+          zoom: 11,
+          center: { lat: 31.432793, lng: 34.387282 }
+        });
+
+    }
+}
+
+function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    var platform = new H.service.Platform({
+      'app_id': 'TkZ5D3cR7A0TNOTm2YI6',
+      'app_code': 'KUbzQcWdvzlcVJrFSQabLQ'
+    });
+
+    var defaultLayers = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    var map = new H.Map(
+      document.getElementById('hereMap'),
+      defaultLayers.normal.map,
+      {
+        zoom: 11,
+        center: { lat: latitude, lng: longitude }
+      });
+
+}
 
 
-map.setBaseLayer(defaultLayers.normal.traffic.incidents);
+
+
+getLocation();
